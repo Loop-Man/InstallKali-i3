1deebbc597c7664a39dbbe886bd5de1e6c0cb415
Loop-Man <60947094+Loop-Man@users.noreply.github.com>
2021-05-03 19:07:48 +0200

    Create i3install.sh

---
+++ i3install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt install i3 -y
+sudo apt install i3-gaps -y
+sudo apt install i3blocks -y
+sudo apt install rofi -y
+sudo apt install htop -y
+sudo apt install ranger -y
+sudo apt install flameshot -y
+sudo apt install picom -y
+sudo apt install feh -y
+sudo apt install calcurse -y
+sudo apt install xfce4-appfinder -y
+sudo apt install nm-connection-editor -y
+sudo apt install unclutter-xfixes -y
+sudo apt install redshift -y
+sudo apt install cbatticon -y
